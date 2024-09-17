e70ba35e5caf5e5d5651b3643b69a2ea21c40677
koirherianto <koir.harianto@gmail.com>
2024-09-17 10:17:03 +0800

    Add routes for pages, topbars, and versions controllers

--- start/routes.ts
+++ start/routes.ts
@@ -4,6 +4,7 @@ const LandingController = () => import('#controllers/landing_controller')
 const AuthController = () => import('#controllers/auth_controller')
 const ProjectsController = () => import('#controllers/projects_controller')
 const ProfilesController = () => import('#controllers/profiles_controller')
+const PagesController = () => import('#controllers/pages_controller')
 import { middleware } from './kernel.js'
 import router from '@adonisjs/core/services/router'
 // router.on('/').renderInertia('home', { version: 6 })
@@ -20,10 +21,13 @@ router.group(() => {
 
 router.group(() => {
     router.delete('/logout', [AuthController, 'logout'])
+
     router.get('/u/:username/project', [ProjectsController, 'index']).as('projects.index')
     router.get('/u/:username/project/create', [ProjectsController, 'create']).as('projects.create')
     router.post('/u/:username/project', [ProjectsController, 'store']).as('projects.store')
 
     router.get('/u/:username/profile', [ProfilesController, 'index']).as('profiles.index')
     router.put('/u/:username/profile', [ProfilesController, 'update']).as('profiles.update')
+
+    router.get(':projectSlug/:versionSlug/:topbarSlug/:pageSlug', [PagesController, 'index']).as('pages.index')
 }).middleware(middleware.auth())
