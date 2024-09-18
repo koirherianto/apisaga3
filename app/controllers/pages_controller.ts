2f32cf61f427b02c300daa19986608755ae68ebb
koirherianto <koir.harianto@gmail.com>
2024-09-18 11:36:36 +0800

    Refactor pages controller and view path

--- app/controllers/pages_controller.ts
+++ app/controllers/pages_controller.ts
@@ -2,6 +2,6 @@ import type { HttpContext } from '@adonisjs/core/http'
 
 export default class PagesController {
   async index({ inertia }: HttpContext) {
-    return inertia.render('editor/index', { version: 6 })
+    return inertia.render('pages/index', { version: 6 })
   }
 }
